f483500413b4e85a069ea9de0f075a860b778bfd
Kevin <king.feruke@gmail.com>
2012-01-09 19:07:50 -0500

    fix memory leak in file watcher

--- logmapper.js
+++ logmapper.js
@@ -5,7 +5,10 @@ var city = new City('./GeoLiteCity.dat');
 var everyone = now.initialize(app);
 var filename = "./log";
 var backlog_size = 2000;
+var stat = null;
+var watch = null;
 
+// send html page back
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
 	console.log('conntected from ' + req.connection.remoteAddress);
@@ -13,41 +16,51 @@ app.get('/', function(req, res) {
 
 // on socket connect
 now.on('connect', function() {
-	fs.stat(filename, function(err, stats) {
-		if (err)
-			throw err;
-		var start = (stats.size > backlog_size) ? (stats.size - backlog_size) : 0;
-		var stream = fs.createReadStream(filename, {
-			start : start,
-			end : stats.size
-		});
-		stream.addListener("data", function(lines) {
-			everyone.now.receiveMessage(formatter(lines));
-		});
-	});
-
-	// file watcher
-	fs.watchFile(filename, function(curr, prev) {
-		if (prev.size > curr.size)
-			return {
-				clear : true
-			};
-		var stream = fs.createReadStream(filename, {
-			start : prev.size,
-			end : curr.size
-		});
-		stream.addListener("data", function(lines) {
-			everyone.now.receiveMessage(formatter(lines));
-		});
-	});
+	fileWatcher();
 });
+
+function fileWatcher(){
+	// Stat initial blocks of file
+	if(stat == null){
+		console.log('stat created');
+		stat = fs.stat(filename, function(err, stats) {
+			if (err)
+				throw err;
+			var start = (stats.size > backlog_size) ? (stats.size - backlog_size) : 0;
+			var stream = fs.createReadStream(filename, {
+				start : start,
+				end : stats.size
+			});
+			stream.addListener("data", function(lines) {
+				everyone.now.receiveMessage(formatter(lines));
+			});
+		});
+	}
+	// file watcher
+	if(watch == null){
+		console.log('watcher created');
+		watch = fs.watchFile(filename, function(curr, prev) {
+			if (prev.size > curr.size)
+				return {
+					clear : true
+				};
+			var stream = fs.createReadStream(filename, {
+				start : prev.size,
+				end : curr.size
+			});
+			stream.addListener("data", function(lines) {
+				everyone.now.receiveMessage(formatter(lines));
+			});
+		});
+	}
+}
 
 // pull out the ips
 function formatter(arr) {
 	arr = arr.toString('utf-8');
 	arr = arr.split("\n");
 	// arr = arr.slice(arr.indexOf("\n") + 1).split("\n");
-	var pattern = /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})/g;
+	var pattern = /((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})/g;
 	var ips = new Array();
 	for ( var i in arr) {
 		var addr = arr[i].match(pattern);
